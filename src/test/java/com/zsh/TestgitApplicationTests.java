a8df0a22fdf3cdd61f6ae6682706fa996e3c1343
Ronin <1817937322@qq.com>
2022-01-23 22:45:24 +0800

    初始化项目

---
+++ src/test/java/com/zsh/TestgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
